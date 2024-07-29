c5ae94c67c09664d712f16c993a8baa3116e7c09
Destiny Saturday <destiny@destinedcodes.com>
2024-07-29 11:37:09 +0100

    Update entrypoint.sh
    Signed-off-by: Destiny Saturday  <destiny@destinedcodes.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,7 +8,7 @@ then
     echo "sshpass could not be found. Please install it."
     exit 1
 fi
-
+echo $SERVER_PORT
 sshpass -p "$SERVER_PASSWORD" ssh -o StrictHostKeyChecking=no -p $SERVER_PORT $SERVER_USERNAME@$SERVER_HOST << EOF
   whoami
   pwd
