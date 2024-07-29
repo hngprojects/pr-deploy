2c749ae41db69bf4e1f9cef6ebd8cbcb77caa870
Destiny Saturday <destiny@destinedcodes.com>
2024-07-29 13:16:46 +0100

    Update entrypoint.sh
    Signed-off-by: Destiny Saturday  <destiny@destinedcodes.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 set -e
 
 sshpass -p "$SERVER_PASSWORD" ssh -o StrictHostKeyChecking=no -p $SERVER_PORT $SERVER_USERNAME@$SERVER_HOST << EOF
-    echo $BRANCH
+    echo $GITHUB_HEAD_REF
     rm -rf $REPO_DIR
     git clone $REPO_URL $REPO_DIR
     cd $REPO_DIR
