1b9ea75ab7c992383a9ca428f1a6d1a81035356f
Dominic <cadatechnonso@gmail.com>
2024-07-30 21:19:07 +0100

    Update entrypoint.sh
    Signed-off-by: Dominic <cadatechnonso@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,7 +33,7 @@ chmod +x pr-deploy.sh
 
 # Define the file to store the comment ID
 FILE_NAME="${REPO_OWNER}_${REPO_NAME}_${GITHUB_HEAD_REF}_${PR_NUMBER}"
-COMMENT_ID_FILE="${GITHUB_WORKSPACE}/${FILE_NAME}.txt"
+# COMMENT_ID_FILE="${GITHUB_WORKSPACE}/${FILE_NAME}.txt"
 
 # Checks if the action is opened
 if [ "$PR_ACTION" == "opened" ]; then
@@ -43,50 +43,34 @@ if [ "$PR_ACTION" == "opened" ]; then
       "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/issues/${PR_NUMBER}/comments")
   
   # Extract the comment ID using jq
-  COMMENT_ID=$(echo "$RESPONSE" | jq -r '.id')
+  # COMMENT_ID=$(echo "$RESPONSE" | jq -r '.id')
   
   # Save the comment ID to a file
-  echo "$COMMENT_ID" > "$COMMENT_ID_FILE"
+  # echo "$COMMENT_ID" > "$COMMENT_ID_FILE"
 fi
-  
+
+# Make an API request to get the comments on the pull request
+RESPONSE_DATA=$(curl -s -H "Authorization: token $GITHUB_TOKEN" \
+    "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/issues/${PR_NUMBER}/comments")
+
+# Parse the response to extract the ID of the first comment made by github-actions
+COMMENT_ID=$(echo "$RESPONSE_DATA" | jq -r '[.[] | select(.user.login == "github-actions")] | first | .id')
+
+
 # Copy the pr-deploy.sh script to the remote server.
 sshpass -p "$SERVER_PASSWORD" scp -o StrictHostKeyChecking=no -P $SERVER_PORT ./pr-deploy.sh $SERVER_USERNAME@$SERVER_HOST:/srv/pr-deploy.sh
 
 # Run the pr-deploy.sh script on the remote server and capture the last line of its output.
 DEPLOYED_URL=$(sshpass -p "$SERVER_PASSWORD" ssh -o StrictHostKeyChecking=no -p $SERVER_PORT $SERVER_USERNAME@$SERVER_HOST /srv/pr-deploy.sh $CONTEXT $DOCKERFILE $EXPOSED_PORT $REPO_URL $REPO_OWNER $REPO_NAME $GITHUB_HEAD_REF $GITHUB_SHA $SERVER_HOST $PR_ACTION $PR_NUMBER | tail -n 1)
 
-
-
-# if [ $PR_ACTION == "opened" ]; then
-#   RESPONSE=$(curl -s -H "Authorization: token $GITHUB_TOKEN" -X POST \
-#       -d "$(jq -n --arg body "$COMMENT" '{body: $body}')" \
-#       "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/issues/${PR_NUMBER}/comments")
-#     # Extract the comment ID using jq
-#   COMMENT_ID=$(echo "$RESPONSE" | jq -r '.id')
-# else
-#   curl -X PATCH -H "Authorization: token $GITHUB_TOKEN" \
-#        -d "$(jq -n --arg body "$COMMENT" '{body: $body}')" \
-#        -H "Accept: application/vnd.github.v3+json" \
-#        "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/issues/comments/$COMMENT_ID" \
-# fi
-
-
 if [  -z "$DEPLOYED_URL" ]; then
-  if [ -f "$COMMENT_ID_FILE" ]; then
-    COMMENT_ID=$(cat "$COMMENT_ID_FILE")
     COMMENT=$(update_comment "FAILED" "") 
     curl -X PATCH -H "Authorization: token $GITHUB_TOKEN" \
          -d "$(jq -n --arg body "$COMMENT" '{body: $body}')" \
          -H "Accept: application/vnd.github.v3+json" \
          "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/issues/comments/$COMMENT_ID" >/dev/null
-    
     echo "Comment ID updated: $COMMENT_ID"
-  else
-    echo "Error: Comment ID file not found."
-  fi
 elif [ "$PR_ACTION" == "opened" ]; then
-    if [ -f "$COMMENT_ID_FILE" ]; then
-    COMMENT_ID=$(cat "$COMMENT_ID_FILE")
     COMMENT=$(update_comment "Deployed" "Preview link") 
 
     curl -X PATCH -H "Authorization: token $GITHUB_TOKEN" \
@@ -95,14 +79,9 @@ elif [ "$PR_ACTION" == "opened" ]; then
          "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/issues/comments/$COMMENT_ID" >/dev/null
     
     echo "Comment ID updated: $COMMENT_ID"
-  else
-    echo "Error: Comment ID file not found."
-  fi
   
 elif [[ "$PR_ACTION" == "synchronize" || "$PR_ACTION" == "reopened" ]]; then
   # Read the comment ID from the file
-  if [ -f "$COMMENT_ID_FILE" ]; then
-    COMMENT_ID=$(cat "$COMMENT_ID_FILE")
     COMMENT=$(update_comment "Deployed" "Preview link")
     curl -X PATCH -H "Authorization: token $GITHUB_TOKEN" \
          -d "$(jq -n --arg body "$COMMENT" '{body: $body}')" \
@@ -110,13 +89,8 @@ elif [[ "$PR_ACTION" == "synchronize" || "$PR_ACTION" == "reopened" ]]; then
          "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/issues/comments/$COMMENT_ID" >/dev/null
     
     echo "Comment ID updated: $COMMENT_ID"
-  else
-    echo "Error: Comment ID file not found."
-  fi
 elif [ "$PR_ACTION" == "closed" ]; then
   # Read the comment ID from the file
-  if [ -f "$COMMENT_ID_FILE" ]; then
-    COMMENT_ID=$(cat "$COMMENT_ID_FILE")
     COMMENT=$(update_comment "Closed" "") 
     curl -X PATCH -H "Authorization: token $GITHUB_TOKEN" \
          -d "$(jq -n --arg body "$COMMENT" '{body: $body}')" \
@@ -124,9 +98,6 @@ elif [ "$PR_ACTION" == "closed" ]; then
          "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/issues/comments/$COMMENT_ID" >/dev/null
     
     echo "Comment ID deleted: $COMMENT_ID"
-  else
-    echo "Error: Comment ID file not found."
-  fi
 fi
 
 # Echo the deployed URL.
