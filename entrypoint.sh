0ab9e53149adf778385a3c323dfa9426acd8af39
Ugochi Ukaegbu <110994907+Zenitugo@users.noreply.github.com>
2024-07-30 01:57:22 +0100

    Update entrypoint.sh
    Signed-off-by: Ugochi Ukaegbu <110994907+Zenitugo@users.noreply.github.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,5 +2,9 @@
 
 set -e
 
-chmod +x ./deploy.sh
+echo "before"
+ls
+chmod +x deploy.sh
+echo "after"
+
 sshpass -p "$SERVER_PASSWORD" ssh -o StrictHostKeyChecking=no -p $SERVER_PORT $SERVER_USERNAME@$SERVER_HOST ./deploy.sh $CONTEXT $DOCKERFILE $EXPOSED_PORT $REPO_URL $REPO_OWNER $REPO_NAME $GITHUB_HEAD_REF $PR_NUMBER $GITHUB_TOKEN
