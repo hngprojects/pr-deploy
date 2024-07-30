0847a886c2823a03e8698ca234cdcbb0ab021c0d
Destiny Saturday <destiny@destinedcodes.com>
2024-07-30 22:42:02 +0100

    Update entrypoint.sh
    Signed-off-by: Destiny Saturday  <destiny@destinedcodes.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,8 +5,7 @@ set -e
 
 # Prepare the comment to be posted on GitHub.
 update_comment() {
-    COMMENT="<strong>Here are the latest updates on your deployment.</strong> Explore the action and ⭐ star our project for more insights! 🔍
-    
+    COMMENT="<strong>Here are the latest updates on your deployment.</strong> Explore and star the PR Deploy action 🤖"    
     <table>
       <thead>
         <tr>
@@ -19,13 +18,12 @@ update_comment() {
       <tbody>
         <tr>
           <td><a href=\"https://github.com/hngprojects/pr-deploy\">PR Deploy</a></td>
-          <td>${1} 🚀</td>
-          <td><a href=\"$DEPLOYED_URL\">${2-""}</a></td>
+          <td>${1}</td>
+          <td><a href=\"${2}\">Visit Preview</a></td>
           <td>$(date +'%b %d, %Y %I:%M%p')</td>
         </tr>  
       </tbody>
     </table>"
-    echo $COMMENT
 }
 
 # Make the pr-deploy.sh script executable.
@@ -37,7 +35,7 @@ FILE_NAME="${REPO_OWNER}_${REPO_NAME}_${GITHUB_HEAD_REF}_${PR_NUMBER}"
 
 # Checks if the action is opened
 if [ "$PR_ACTION" == "opened" ]; then
-  COMMENT=$(update_comment "starting deployment..." "") 
+  COMMENT=$(update_comment "Deploying ⏳" "#") 
   curl -s -H "Authorization: token $GITHUB_TOKEN" -X POST \
       -d "$(jq -n --arg body "$COMMENT" '{body: $body}')" \
       "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/issues/${PR_NUMBER}/comments"
@@ -59,41 +57,21 @@ sshpass -p "$SERVER_PASSWORD" scp -o StrictHostKeyChecking=no -P $SERVER_PORT ./
 DEPLOYED_URL=$(sshpass -p "$SERVER_PASSWORD" ssh -o StrictHostKeyChecking=no -p $SERVER_PORT $SERVER_USERNAME@$SERVER_HOST /srv/pr-deploy.sh $CONTEXT $DOCKERFILE $EXPOSED_PORT $REPO_URL $REPO_OWNER $REPO_NAME $GITHUB_HEAD_REF $GITHUB_SHA $SERVER_HOST $PR_ACTION $PR_NUMBER | tail -n 1)
 
 if [  -z "$DEPLOYED_URL" ]; then
-    COMMENT=$(update_comment "FAILED" "") 
+    COMMENT=$(update_comment "Failed ❌" "#") 
     curl -X PATCH -H "Authorization: token $GITHUB_TOKEN" \
          -d "$(jq -n --arg body "$COMMENT" '{body: $body}')" \
          -H "Accept: application/vnd.github.v3+json" \
          "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/issues/comments/$COMMENT_ID" >/dev/null
-    echo "Comment ID updated: $COMMENT_ID"
-elif [ "$PR_ACTION" == "opened" ]; then
-    COMMENT=$(update_comment "Deployed" "Preview link") 
-
-    curl -X PATCH -H "Authorization: token $GITHUB_TOKEN" \
-         -d "$(jq -n --arg body "$COMMENT" '{body: $body}')" \
-         -H "Accept: application/vnd.github.v3+json" \
-         "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/issues/comments/$COMMENT_ID" >/dev/null
-    
-    echo "Comment ID updated: $COMMENT_ID"
-  
-elif [[ "$PR_ACTION" == "synchronize" || "$PR_ACTION" == "reopened" ]]; then
-  # Read the comment ID from the file
-    COMMENT=$(update_comment "Deployed" "Preview link")
-    curl -X PATCH -H "Authorization: token $GITHUB_TOKEN" \
-         -d "$(jq -n --arg body "$COMMENT" '{body: $body}')" \
-         -H "Accept: application/vnd.github.v3+json" \
-         "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/issues/comments/$COMMENT_ID" >/dev/null
-    
-    echo "Comment ID updated: $COMMENT_ID"
 elif [ "$PR_ACTION" == "closed" ]; then
-  # Read the comment ID from the file
-    COMMENT=$(update_comment "Closed" "") 
+    COMMENT=$(update_comment "Terminated 🛑" "#") 
     curl -X PATCH -H "Authorization: token $GITHUB_TOKEN" \
          -d "$(jq -n --arg body "$COMMENT" '{body: $body}')" \
          -H "Accept: application/vnd.github.v3+json" \
          "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/issues/comments/$COMMENT_ID" >/dev/null
-    
-    echo "Comment ID deleted: $COMMENT_ID"
+else
+    COMMENT=$(update_comment "Deployed 🎉" $DEPLOYED_URL) 
+    curl -X PATCH -H "Authorization: token $GITHUB_TOKEN" \
+         -d "$(jq -n --arg body "$COMMENT" '{body: $body}')" \
+         -H "Accept: application/vnd.github.v3+json" \
+         "https://api.github.com/repos/${REPO_OWNER}/${REPO_NAME}/issues/comments/$COMMENT_ID" >/dev/null
 fi
-
-# Echo the deployed URL.
-echo "Deployed URL: $DEPLOYED_URL"
