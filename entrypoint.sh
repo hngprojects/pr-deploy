360eb27c2fa31bd7a12bcd6c7aae8fc6cca38956
Destiny <yiradesat@gmail.com>
2024-07-30 03:08:50 +0100

    fix: djdj

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,6 @@ sshpass -p "$SERVER_PASSWORD" scp -o StrictHostKeyChecking=no -P $SERVER_PORT ./
 
 DEPLOYED_URL=$(sshpass -p "$SERVER_PASSWORD" ssh -o StrictHostKeyChecking=no -p $SERVER_PORT $SERVER_USERNAME@$SERVER_HOST /srv/pr-deploy.sh $CONTEXT $DOCKERFILE $EXPOSED_PORT $REPO_URL $REPO_OWNER $REPO_NAME $GITHUB_HEAD_REF $PR_NUMBER)
 
-curl ifconfig.me
-
-curl -s -H "Authorization: token $GITHUB_TOKEN" \
--X POST \
--d "{\"body\": \"Deployed URL: $DEPLOYED_URL\"}" \
-"https://api.github.com/repos/hngprojects/pr-deploy/issues/15/comments"
+curl -s -H "Authorization: token $GITHUB_TOKEN" -X POST -d "{\"body\": \"Deployed URL: $DEPLOYED_URL\"}" "https://api.github.com/repos/hngprojects/pr-deploy/issues/15/comments"
 
 echo "Deployed URL: $DEPLOYED_URL"
