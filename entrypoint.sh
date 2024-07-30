b2a05ecb1d1f9236467431e8334ea5daa34d7eb6
Destiny <yiradesat@gmail.com>
2024-07-30 03:36:30 +0100

    style comment

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,27 @@ chmod +x pr-deploy.sh
 
 sshpass -p "$SERVER_PASSWORD" scp -o StrictHostKeyChecking=no -P $SERVER_PORT ./pr-deploy.sh $SERVER_USERNAME@$SERVER_HOST:/srv/pr-deploy.sh
 
-DEPLOYED_URL=$(sshpass -p "$SERVER_PASSWORD" ssh -o StrictHostKeyChecking=no -p $SERVER_PORT $SERVER_USERNAME@$SERVER_HOST /srv/pr-deploy.sh $CONTEXT $DOCKERFILE $EXPOSED_PORT $REPO_URL $REPO_OWNER $REPO_NAME $GITHUB_HEAD_REF $PR_NUMBER)
+DEPLOYED_URL=$(sshpass -p "$SERVER_PASSWORD" ssh -o StrictHostKeyChecking=no -p $SERVER_PORT $SERVER_USERNAME@$SERVER_HOST /srv/pr-deploy.sh $CONTEXT $DOCKERFILE $EXPOSED_PORT $REPO_URL $REPO_OWNER $REPO_NAME $GITHUB_HEAD_REF $PR_NUMBER | tail -n 1)
+
+COMMENT="
+<table>
+  <tr>
+    <th>Deoloyed Branch</th>
+    <th>Status</th>
+    <th>Preview URL</th>
+    <th>Updated At</th>
+  </tr>
+  <tr>
+    <td>$GITHUB_HEAD_REF</td>
+    <td>Deployed</td>
+    <td><a href=\"$DEPLOYED_URL\">$DEPLOYED_URL</a></td>
+    <td>$(date)</td>
+  </tr>
+</table>
+"
 
 curl -s -H "Authorization: token $GITHUB_TOKEN" -X POST \
-    -d "$(jq -n --arg body "Deployed URL: $DEPLOYED_URL" '{body: $body}')" \
+    -d "$(jq -n --arg body "Deployed URL: $COMMENT" '{body: $body}')" \
     "https://api.github.com/repos/hngprojects/pr-deploy/issues/15/comments"
 
 echo "Deployed URL: $DEPLOYED_URL"
