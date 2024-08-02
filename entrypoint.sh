22946e920a04cccbfade330a487a97eab9c060c3
Destiny Saturday <destiny@destinedcodes.com>
2024-08-02 17:22:07 +0100

    Update entrypoint.sh
    Signed-off-by: Destiny Saturday  <destiny@destinedcodes.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,5 @@ set -e
 sshpass -p "$SERVER_PASSWORD" scp -o StrictHostKeyChecking=no -P $SERVER_PORT pr-deploy.sh $SERVER_USERNAME@$SERVER_HOST:/srv/pr-deploy.sh
 
 # Run the pr-deploy.sh script on the remote server.
+echo "REPO_URL: $REPO_URL"
 sshpass -p "$SERVER_PASSWORD" ssh -o StrictHostKeyChecking=no -p $SERVER_PORT $SERVER_USERNAME@$SERVER_HOST 'REPO_URL=$REPO_URL; bash /srv/pr-deploy.sh'
