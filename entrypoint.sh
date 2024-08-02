5182e13ef7aaabbdf19d7c251f2c4685c563105e
Destiny Saturday <destiny@destinedcodes.com>
2024-08-02 17:41:20 +0100

    Update entrypoint.sh
    Signed-off-by: Destiny Saturday  <destiny@destinedcodes.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,9 +4,12 @@
 set -e
 
 # Copy the pr-deploy.sh script to the remote server.
-sshpass -p "$SERVER_PASSWORD" scp -o StrictHostKeyChecking=no -P $SERVER_PORT pr-deploy.sh $SERVER_USERNAME@$SERVER_HOST:/srv/pr-deploy.sh
+# Create a remote script that sets the variable and runs the main script.
+echo '#!/bin/bash' > remote_script.sh
+echo "REPO_URL='$REPO_URL'" >> remote_script.sh
+echo 'chmod +x /srv/pr-deploy.sh' >> remote_script.sh
+echo '/srv/pr-deploy.sh' >> remote_script.sh
 
-# Run the pr-deploy.sh script on the remote server.
-echo "REPO_URL: $REPO_URL"
-sshpass -p "$SERVER_PASSWORD" ssh -o StrictHostKeyChecking=no -p $SERVER_PORT $SERVER_USERNAME@$SERVER_HOST \
-  "REPO_URL='$REPO_URL'; chmod +x /srv/pr-deploy.sh; /srv/pr-deploy.sh"
+# Copy and execute the remote script.
+sshpass -p "$SERVER_PASSWORD" scp -o StrictHostKeyChecking=no -P $SERVER_PORT remote_script.sh $SERVER_USERNAME@$SERVER_HOST:/root/remote_script.sh
+sshpass -p "$SERVER_PASSWORD" ssh -o StrictHostKeyChecking=no -p $SERVER_PORT $SERVER_USERNAME@$SERVER_HOST 'bash /root/remote_script.sh'
