50f3aeb24800822a2c2fafe5a3469cc8b2167ad4
Destiny Saturday <destiny@destinedcodes.com>
2024-08-02 18:37:26 +0100

    Update entrypoint.sh
    Signed-off-by: Destiny Saturday  <destiny@destinedcodes.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,21 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-# Copy and execute the remote script.
+# Copy the script to the remote server.
 sshpass -p "$SERVER_PASSWORD" scp -o StrictHostKeyChecking=no -P $SERVER_PORT pr-deploy.sh $SERVER_USERNAME@$SERVER_HOST:/srv/pr-deploy.sh
-sshpass -p "$SERVER_PASSWORD" ssh -o StrictHostKeyChecking=no -p $SERVER_PORT $SERVER_USERNAME@$SERVER_HOST "BRANCH=$GITHUB_HEAD_REF REPO_URL=$REPO_URL bash /srv/pr-deploy.sh"
+
+# Run the script on the remote server with environment variables.
+sshpass -p "$SERVER_PASSWORD" ssh -o StrictHostKeyChecking=no -p $SERVER_PORT $SERVER_USERNAME@$SERVER_HOST \
+  "GITHUB_TOKEN='$GITHUB_TOKEN' \
+  CONTEXT='$CONTEXT' \
+  DOCKERFILE='$DOCKERFILE' \
+  EXPOSED_PORT='$EXPOSED_PORT' \
+  REPO_OWNER='$REPO_OWNER' \
+  REPO_NAME='$REPO_NAME' \
+  REPO_URL='$REPO_URL' \
+  REPO_ID='$REPO_ID' \
+  BRANCH='$GITHUB_HEAD_REF' \
+  PR_ACTION='$PR_ACTION' \
+  PR_NUMBER='$PR_NUMBER' \
+  COMMENT_ID='$COMMENT_ID' \
+  bash /srv/pr-deploy.sh"
