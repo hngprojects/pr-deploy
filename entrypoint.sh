affc75b2a11a09df76863b1f0997c672b515db11
Destiny Saturday <destiny@destinedcodes.com>
2024-08-03 05:42:12 +0100

    feat: stream logs

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,7 +9,7 @@ LOCAL_LOG_FILE="deployment_output.log"
 # Copy the script to the remote server.
 sshpass -p "$SERVER_PASSWORD" scp -o StrictHostKeyChecking=no -P $SERVER_PORT pr-deploy.sh $SERVER_USERNAME@$SERVER_HOST:/srv/pr-deploy.sh >/dev/null
 
-# Run the script on the remote server, redirecting output and errors to a log file, and capture the output locally
+# Stream the output from the remote script to local terminal and save it to a log file
 sshpass -p "$SERVER_PASSWORD" ssh -o StrictHostKeyChecking=no -p $SERVER_PORT $SERVER_USERNAME@$SERVER_HOST \
   "GITHUB_TOKEN='$GITHUB_TOKEN' \
   CONTEXT='$CONTEXT' \
@@ -24,18 +24,11 @@ sshpass -p "$SERVER_PASSWORD" ssh -o StrictHostKeyChecking=no -p $SERVER_PORT $S
   PR_ACTION='$PR_ACTION' \
   PR_NUMBER='$PR_NUMBER' \
   COMMENT_ID='$COMMENT_ID' \
-  bash /srv/pr-deploy.sh" > "$LOCAL_LOG_FILE" 2>&1
+  bash /srv/pr-deploy.sh" | tee "$LOCAL_LOG_FILE"
 
-# Output the contents of the local log file
-echo "Deployment output:"
-cat "$LOCAL_LOG_FILE"
-
-# Extract the last line from the log file, assuming it contains the deployment URL
+# Output the last line of the log file, assuming it contains the deployment URL
 DEPLOYMENT_URL=$(tail -n 1 "$LOCAL_LOG_FILE")
 
 # Output the deployment URL
 echo "Deployment URL: $DEPLOYMENT_URL"
-
-# Clean up the log file
-rm "$LOCAL_LOG_FILE"
 
