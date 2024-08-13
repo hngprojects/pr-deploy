b889243e99ff85eb3becfe8b376cd382ca2310f8
Destiny Saturday <destiny@destinedcodes.com>
2024-08-13 02:48:06 +0100

    Update entrypoint.sh
    Signed-off-by: Destiny Saturday  <destiny@destinedcodes.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,16 +3,7 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-# Set up SSH command and SCP options based on whether a private key or password is provided
-if [ -n "$SERVER_PRIVATE_KEY" ]; then
-    echo "$SERVER_PRIVATE_KEY" > private_key.pem
-    chmod 600 private_key.pem
-    SSH_CMD="ssh -i private_key.pem -o StrictHostKeyChecking=no -p $SERVER_PORT $SERVER_USERNAME@$SERVER_HOST"
-else
-    SSH_CMD="sshpass -p $SERVER_PASSWORD ssh -o StrictHostKeyChecking=no -p $SERVER_PORT $SERVER_USERNAME@$SERVER_HOST"
-fi
-
-$SSH_CMD \
+sshpass -p $SERVER_PASSWORD ssh -o StrictHostKeyChecking=no -p $SERVER_PORT $SERVER_USERNAME@$SERVER_HOST \
   "GITHUB_TOKEN='$GITHUB_TOKEN' \
   CONTEXT='$CONTEXT' \
   DOCKERFILE='$DOCKERFILE' \
