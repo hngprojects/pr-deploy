683f42d1a996cefbb3bfddf675995a157ea098fd
Destiny Saturday <destiny@destinedcodes.com>
2024-08-13 10:46:53 +0100

    Update pr-deploy.sh
    Signed-off-by: Destiny Saturday  <destiny@destinedcodes.com>

--- pr-deploy.sh
+++ pr-deploy.sh
@@ -2,44 +2,44 @@
 
 set -e
 
-# Define cleanup function
-cleanup() {
+# Function to clean up old containers and images
+cleanup_old_images() {
     CONTAINER_ID=$(docker ps -aq --filter "name=${PR_ID}")
     [ -n "$CONTAINER_ID" ] && docker stop -t 0 "$CONTAINER_ID" && docker rm -f "$CONTAINER_ID"
 
-    IMAGE_ID=$(docker images -q --filter "reference=${PR_ID}")
-    [ -n "$IMAGE_ID" ] && docker rmi -f "$IMAGE_ID"
-    
+    IMAGE_IDS=$(docker images -q --filter "reference=${PR_ID}")
+    for IMAGE_ID in $IMAGE_IDS; do
+        if [ "$IMAGE_ID" != "$NEW_IMAGE_ID" ]; then
+            docker rmi -f "$IMAGE_ID"
+        fi
+    done
     rm -rf $PR_ID
 }
-
-# Check PR action
+cleanup_old_images
 if [ "$PR_ACTION" == "closed" ]; then
-    cleanup
+    # Stop and remove container and image, and delete directory
+    cleanup_old_images
     exit 0
 fi
 
-# Perform cleanup for 'opened' and 'synchronized' actions
-# Only remove the old container if it exists, but keep the image if possible
-CONTAINER_ID=$(docker ps -aq --filter "name=${PR_ID}")
-[ -n "$CONTAINER_ID" ] && docker stop -t 0 "$CONTAINER_ID" && docker rm -f "$CONTAINER_ID"
-
-# Update repository on the server
-rm -rf $PR_ID
+# Clone repository on the server
 git clone -b $BRANCH $REPO_URL $PR_ID
 cd $PR_ID
 
 # Free port
 FREE_PORT=$(python3 -c 'import socket; s = socket.socket(); s.bind(("", 0)); print(s.getsockname()[1]); s.close()')
 
-# Build Docker Image
-docker build --cache-from $PR_ID -t $PR_ID -f $DOCKERFILE $CONTEXT
+# Unzip the Image file and run Docker Container
+docker build -t $PR_ID -f $DOCKERFILE $CONTEXT
 
-# Save environment variables
+# Clean up old images except for the newly created one
+cleanup_old_images
+
 echo $ENVS > "/tmp/${PR_ID}.env"
-
-# Run Docker Container
 docker run -d --env-file "/tmp/${PR_ID}.env" -p $FREE_PORT:$EXPOSED_PORT --name $PR_ID $PR_ID
+
+# Get the newly created image ID
+NEW_IMAGE_ID=$(docker images -q $PR_ID)
 
 # Start SSH Tunnel
 nohup ssh -tt -o StrictHostKeyChecking=no -R 80:localhost:$FREE_PORT serveo.net > serveo_output.log 2>&1 &
